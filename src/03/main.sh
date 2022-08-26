1faebb92c71ac0943b381a817725be1e49d715b6
Yonn Argelia <yonnarge@student.21-school.ru>
2022-08-26 20:20:10 +0300

    Part 3 is done

---
+++ src/03/main.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#
+# Copyright (с) 2022 Student of School 21:
+# Yonn Argelia
+#
+# yonnarge@student.21-school.ru
+#
+
+. ./print.sh
+
+color_codes="[1-6]{1}"
+
+if [ $# -ne 4 ] ; then
+  echo "Please run script with four different parameters from 1 to 6"
+
+else
+  if [[ $1 =~ $color_codes ]] &&
+     [[ $2 =~ $color_codes ]] &&
+     [[ $3 =~ $color_codes ]] &&
+     [[ $4 =~ $color_codes ]] ; then
+
+    if [[ $1 == $2 ]] || [[ $3 == $4 ]] ; then
+      echo "First parameter can't be equal second and third can't be equal fourth, please try again"
+    else
+      HOSTNAME=$(hostname)
+      TIMEZONE="$(cat /etc/timezone) UTC $(date +%:::z)"
+      USER=$USER
+      OS=$(lsb_release -d | awk '{ for (i = 2; i <= NF; i++) {printf "%s ",$i} {printf "\n"}}')
+      DATE=$(date "+%d %B %Y %H:%M:%S")
+      UPTIME=$(uptime | awk '{print $3}' | tr -s ',' ' ')
+      UPTIME_SEC=$(cat /proc/uptime | awk '{print $1}')
+      IP=$(ifconfig | awk '{if ($0 ~ /inet/) print $2}' | awk 'NR == 1')
+      NETMASK=$(ifconfig | awk '{if ($0 ~ /netmask/) print $4}' | awk 'NR == 1')
+      GATEWAY=$(route -n | awk '$1 ~ /0.0.0.0/' | awk '{print $2}')
+      RAM_TOTAL=$(free | awk '/Mem:/ {printf "%.3f GB\n", $2/1024/1024}')
+      RAM_USED=$(free | awk '/Mem:/ {printf "%.3f GB\n", $3/1024/1024}')
+      RAM_FREE=$(free | awk '/Mem:/ {printf "%.3f GB\n", $4/1024/1024}')
+      SPACE_ROOT=$(df / | awk '/\// {printf "%.2f MB\n", $2/1024}')
+      SPACE_ROOT_USED=$(df / | awk '/\// {printf "%.2f MB\n", $3/1024}')
+      SPACE_ROOT_FREE=$(df / | awk '/\// {printf "%.2f MB\n", $4/1024}')
+
+      set_code_color $1
+      name_bg_color="\033[4$?m"
+      set_code_color $2
+      name_tx_color="\033[3$?m"
+      set_code_color $3
+      value_bg_color="\033[4$?m"
+      set_code_color $4
+      value_tx_color="\033[3$?m"
+
+      default_color="\033[0m"
+
+      print
+
+    fi
+  else
+    echo "Please use different parameters from 1 to 6"
+  fi
+fi
---
+++ src/03/print.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#
+# Copyright (с) 2022 Student of School 21:
+# Yonn Argelia
+#
+# yonnarge@student.21-school.ru
+#
+
+function set_color_code() {
+  code=0
+
+  case "$1" in
+    1) code=7 ;;
+    2) code=1 ;;
+    3) code=2 ;;
+    4) code=4 ;;
+    5) code=5 ;;
+    6) code=0 ;;
+  esac
+
+  return $code
+}
+
+function print() {
+  echo "${name_bg_color}${name_tx_color}HOSTNAME${default_color} = ${value_bg_color}${value_tx_color}$HOSTNAME${default_color}"
+  echo "${name_bg_color}${name_tx_color}TIMEZONE${default_color} = ${value_bg_color}${value_tx_color}$TIMEZONE${default_color}"
+  echo "${name_bg_color}${name_tx_color}USER${default_color} = ${value_bg_color}${value_tx_color}$USER${default_color}"
+  echo "${name_bg_color}${name_tx_color}OS${default_color} = ${value_bg_color}${value_tx_color}$OS${default_color}"
+  echo "${name_bg_color}${name_tx_color}DATE${default_color} = ${value_bg_color}${value_tx_color}$DATE${default_color}"
+  echo "${name_bg_color}${name_tx_color}UPTIME${default_color} = ${value_bg_color}${value_tx_color}$UPTIME${default_color}"
+  echo "${name_bg_color}${name_tx_color}UPTIME_SEC${default_color} = ${value_bg_color}${value_tx_color}$UPTIME_SEC${default_color}"
+  echo "${name_bg_color}${name_tx_color}IP${default_color} = ${value_bg_color}${value_tx_color}$IP${default_color}"
+  echo "${name_bg_color}${name_tx_color}NETMASK${default_color} = ${value_bg_color}${value_tx_color}$NETMASK${default_color}"
+  echo "${name_bg_color}${name_tx_color}GATEWAY${default_color} = ${value_bg_color}${value_tx_color}$GATEWAY${default_color}"
+  echo "${name_bg_color}${name_tx_color}RAM_TOTAL${default_color} = ${value_bg_color}${value_tx_color}$RAM_TOTAL${default_color}"
+  echo "${name_bg_color}${name_tx_color}RAM_USED${default_color} = ${value_bg_color}${value_tx_color}$RAM_USED${default_color}"
+  echo "${name_bg_color}${name_tx_color}RAM_FREE${default_color} = ${value_bg_color}${value_tx_color}$RAM_FREE${default_color}"
+  echo "${name_bg_color}${name_tx_color}SPACE_ROOT${default_color} = ${value_bg_color}${value_tx_color}$SPACE_ROOT${default_color}"
+  echo "${name_bg_color}${name_tx_color}SPACE_ROOT_USED${default_color} = ${value_bg_color}${value_tx_color}$SPACE_ROOT_USED${default_color}"
+  echo "${name_bg_color}${name_tx_color}SPACE_ROOT_FREE${default_color} = ${value_bg_color}${value_tx_color}$SPACE_ROOT_FREE${default_color}"
+
+  return 1
+}
