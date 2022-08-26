f669a6d7da230fdc7e1949506e95fce0127c3b29
Yonn Argelia <yonnarge@student.21-school.ru>
2022-08-26 17:23:45 +0300

    Fix Part 2

--- src/02/print.sh
+++ src/02/print.sh
@@ -20,7 +20,7 @@ function print() {
   echo "GATEWAY = $GATEWAY"
   echo "RAM_TOTAL = $RAM_TOTAL"
   echo "RAM_USED = $RAM_USED"
-  echo "RAM_TOTAL = $RAM_TOTAL"
+  echo "RAM_FREE = $RAM_FREE"
   echo "SPACE_ROOT = $SPACE_ROOT"
   echo "SPACE_ROOT_USED = $SPACE_ROOT_USED"
   echo "SPACE_ROOT_FREE = $SPACE_ROOT_FREE"
